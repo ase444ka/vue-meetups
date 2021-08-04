0dcd12c6a1db3b5431225177e2eb25638eeb9be6
Istomina Asya <istomina.asia@yandex.ru>
2021-08-04 17:52:13 +0300

    Router setup -added scrollBehavior -added children to meetup page -added '@' alias to paths

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,24 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
+export function scrollBehavior(to, from, savedPosition) {
+  let position = {};
+
+  if (
+    to.matched.some((route) => route.meta && route.meta.saveScrollPosition) &&
+    from.matched.some((route) => route.meta && route.meta.saveScrollPosition)
+  ) {
+    position = false;
+  } else if (to.hash) {
+    position.selector = to.hash;
+  } else if (savedPosition) {
+    position = savedPosition;
+  } else {
+    position = { x: 0, y: 0 };
+  }
+  return position;
+}
+
 export const router = new VueRouter({
   mode: 'history',
 
@@ -11,34 +29,34 @@ export const router = new VueRouter({
     {
       path: '/',
       name: 'index',
-      component: () => import('../views/MeetupsPage'),
+      component: () => import('@/views/MeetupsPage'),
     },
 
     {
       path: '/meetups',
       name: 'meetups',
-      component: () => import('../views/MeetupsPage'),
+      component: () => import('@/views/MeetupsPage'),
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/LoginPage'),
+      component: () => import('@/views/LoginPage'),
     },
     {
       path: '/register',
       name: 'register',
-      component: () => import('../views/LoginPage'),
+      component: () => import('@/views/LoginPage'),
     },
     {
       path: '/meetups/create',
       name: 'form',
-      component: () => import('../views/FormPage'),
+      component: () => import('@/views/FormPage'),
     },
     {
       path: '/meetups/:meetupId',
       name: 'meetup',
       props: true,
-      component: () => import('../views/MeetupPage'),
+      component: () => import('@/views/MeetupPage'),
       meta: {
         showReturnToMeetups: true,
       },
@@ -46,6 +64,18 @@ export const router = new VueRouter({
         if (typeof to.params.meetupId === 'number') next();
         else next(false);
       },
+      children: [
+        {
+          path: '/meetups/:meetupId/description',
+          name: 'description',
+          component: () => import('@/components/MeetupDescription'),
+        },
+        {
+          path: '/meetups/:meetupId/agenda',
+          name: 'agenda',
+          component: () => import('@/components/MeetupAgenda'),
+        },
+      ],
     },
   ],
 });
