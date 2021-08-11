4c132c0b6c880dca4d4257021084cbb026d5c72d
Istomina Asya <istomina.asia@yandex.ru>
2021-08-11 18:11:31 +0300

    AllPages: add styles

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-import './assets/styles/app.css';
+import './assets/styles/index.css';
 import { router } from './router';
 
 Vue.config.productionTip = false;
